2343accb17c443cb690818f95a064220eb3348b8
Kate Makarova <kate.makarova@hotmail.com>
2026-03-26 18:29:27 -0400

    spoiler-box

--- src/Services/bb_codes.go
+++ src/Services/bb_codes.go
@@ -199,10 +199,12 @@ func GetBBCompiler() bbcode.Compiler {
 
 		header := bbcode.NewHTMLTag(html.EscapeString(title))
 		header.Name = "div"
+		header.Attrs["class"] = "spoiler-header"
 		out.AppendChild(header)
 
 		content := bbcode.NewHTMLTag("")
 		content.Name = "div"
+		content.Attrs["class"] = "spoiler-content"
 		content.Attrs["style"] = "max-height: 0; overflow: hidden; transition: max-height 0.3s ease;"
 		out.AppendChild(content)
 
